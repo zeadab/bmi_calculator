871c3f5cc213e4fd72430dc235a1fdd1d86f0173
zeadab <ist.abadi.ali@gmail.com>
2024-03-25 14:57:00 +0100

    adjusting the color of cards and using Reusablecard widget instead of conatainers

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+const containerCArdColor = Color(0XFF1D1E33);
+const buttomContainerHeight=80.0;
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -17,19 +18,26 @@ class _InputPageState extends State<InputPage> {
           Expanded(
               child: Row(
             children: [
-              Expanded(child: Reusablecard(mcolor: Color(0XFF1D1E33))),
-              Expanded(child: Reusablecard(mcolor: Color(0XFF1D1E33))),
+              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
+              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
             ],
           )),
-          Expanded(child: Reusablecard(mcolor: Color(0XFF1D1E33))),
+          Expanded(child: Reusablecard(mcolor: containerCArdColor)),
           Expanded(
               child: Row(
             children: [
-              Expanded(child: Reusablecard(mcolor: Color(0XFF1D1E33))),
-              Expanded(child: Reusablecard(mcolor: Color(0XFF1D1E33))),
+              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
+              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
             ],
           )),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            color:  Color(0xffeb1555),
+            height: buttomContainerHeight,
+          )
         ],
+
       ),
     );
   }
@@ -45,7 +53,7 @@ class Reusablecard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(10.0),
         color: mcolor,
       ),
     );
