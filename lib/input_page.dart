ae649557c50204e4158556c232d59a82cbaa5e52
zeadab <ist.abadi.ali@gmail.com>
2024-04-15 23:33:35 +0200

    the top card are now dynamic in being active or inactive , a GestureDitector is addes to ReasbleCard , Ternary Operaters are added for color of top cards

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,14 +2,17 @@ import 'package:bmi_calculator/widgets/Cardinhalt.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/widgets/REusablecard.dart';
-const containerCArdColor = Color(0XFF1D1E33);
-const buttomContainerHeight=80.0;
+
+const buttomContainerHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender? selecteGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,35 +23,53 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
               child: Row(
-            children: [
-              Expanded(child: Reusablecard(mcolor: containerCArdColor,
-              cardChild:  cardInhalt(icon:FontAwesomeIcons.mars,label:"MALE"),
+                children: [
+                  Expanded(
+                      child: Reusablecard(
+                        mcolor: maleCardColor =
+                        selecteGender == Gender.male
+                            ? activecardcolor
+                            : containerCArdColor,
+                        cardChild:
+                        cardInhalt(icon: FontAwesomeIcons.mars, label: "MALE"),
+                        onpress: () {
+                          setState(() {
+                            selecteGender = Gender.male;
+                          });
+                        },
+                      )),
+                  Expanded(
+                      child: Reusablecard(
+                        mcolor: femaleCardColor = selecteGender == Gender.female
+                            ? activecardcolor
+                            : containerCArdColor,
+                        cardChild:
+                        cardInhalt(
+                            icon: FontAwesomeIcons.venus, label: "FEMALE"),
+                        onpress: () {
+                          setState(() {
+                            selecteGender = Gender.female;
+                          });
+                        },
+                      )),
+                ],
               )),
-              Expanded(child: Reusablecard(mcolor: containerCArdColor
-              ,
-                  cardChild:  cardInhalt(icon:FontAwesomeIcons.venus,label:"FEMALE")
-              )),
-            ],
-          )),
           Expanded(child: Reusablecard(mcolor: containerCArdColor)),
           Expanded(
               child: Row(
-            children: [
-              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
-              Expanded(child: Reusablecard(mcolor: containerCArdColor)),
-            ],
-          )),
+                children: [
+                  Expanded(child: Reusablecard(mcolor: containerCArdColor)),
+                  Expanded(child: Reusablecard(mcolor: containerCArdColor)),
+                ],
+              )),
           Container(
             margin: const EdgeInsets.only(top: 10),
             width: double.infinity,
-            color:  const Color(0xffeb1555),
+            color: const Color(0xffeb1555),
             height: buttomContainerHeight,
           )
         ],
-
       ),
     );
   }
 }
-
-
--- lib/widgets/REusablecard.dart
+++ lib/widgets/REusablecard.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 
-class Reusablecard extends StatelessWidget {
-  Reusablecard({required this.mcolor, this.cardChild});
+const activecardcolor = Color(341818);
+const containerCArdColor = Color(0XFF1D1E33);
 
+enum Gender { male, female }
+
+Color maleCardColor = containerCArdColor;
+Color femaleCardColor = containerCArdColor;
+
+class Reusablecard extends StatelessWidget {
+  Reusablecard({required this.mcolor, this.cardChild, this.onpress});
+
+  final Function()? onpress;
   final Color mcolor;
   final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: mcolor,
-      ),
-      child: cardChild,
-    );
+    return GestureDetector(
+        onTap: onpress,
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: mcolor,
+          ),
+          child: cardChild,
+        ));
   }
 }
